d86a12587b0c50e79a55f3f03612128d534bcb45
Luizfpbp <luizfpbp@gmail.com>
2025-06-20 11:36:19 -0300

    feat: users and book table

--- scripts.js
+++ scripts.js
@@ -1,145 +1,95 @@
-/*
-  --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
-  --------------------------------------------------------------------------------------
-*/
-const getList = async () => {
-  let url = 'http://127.0.0.1:5000/produtos';
-  fetch(url, {
-    method: 'get',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.produtos.forEach(item => insertList(item.nome, item.quantidade, item.valor))
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+window.addEventListener("DOMContentLoaded", () => {
+  getUsersList();
+  getBooksList();
+  getBookOptions();
+});
+
+// Navigation
+function navigate(page) {
+  document.querySelectorAll(".page").forEach((p) => (p.style.display = "none"));
+  document.getElementById(page).style.display = "block";
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
-  --------------------------------------------------------------------------------------
-*/
-getList()
+// Services
+const UserService = {
+  getUsersList: async () => {
+    const response = await fetch("http://127.0.0.1:5000/usuarios");
+    return await response.json();
+  },
+};
 
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para colocar um item na lista do servidor via requisição POST
-  --------------------------------------------------------------------------------------
-*/
-const postItem = async (inputProduct, inputQuantity, inputPrice) => {
-  const formData = new FormData();
-  formData.append('nome', inputProduct);
-  formData.append('quantidade', inputQuantity);
-  formData.append('valor', inputPrice);
-
-  let url = 'http://127.0.0.1:5000/produto';
-  fetch(url, {
-    method: 'post',
-    body: formData
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para criar um botão close para cada item da lista
-  --------------------------------------------------------------------------------------
-*/
-const insertButton = (parent) => {
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  parent.appendChild(span);
-}
-
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para remover um item da lista de acordo com o click no botão close
-  --------------------------------------------------------------------------------------
-*/
-const removeElement = () => {
-  let close = document.getElementsByClassName("close");
-  // var table = document.getElementById('myTable');
-  let i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement.parentElement;
-      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-      if (confirm("Você tem certeza?")) {
-        div.remove()
-        deleteItem(nomeItem)
-        alert("Removido!")
-      }
+const BookService = {
+  getBooksList: async (disponivel) => {
+    url = "http://127.0.0.1:5000/livros";
+    if (disponivel) {
+      url = url + `?disponivel=${disponivel}`;
     }
+
+    const response = await fetch(url);
+    return await response.json();
+  },
+};
+
+// User scripts
+const getUsersList = async () => {
+  try {
+    const data = await UserService.getUsersList();
+    data.values.forEach((item) => insertUsersCard(item));
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
   }
+};
+
+function insertUsersCard(item) {
+  const tabela = document.getElementById("userList");
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.email}</td>
+    <td>${item.data_nascimento}</td>
+  `;
+  tabela.appendChild(tr);
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para deletar um item da lista do servidor via requisição DELETE
-  --------------------------------------------------------------------------------------
-*/
-const deleteItem = (item) => {
-  console.log(item)
-  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
-  fetch(url, {
-    method: 'delete'
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
+// Book scripts
+const getBooksList = async () => {
+  try {
+    const data = await BookService.getBooksList();
+    data.values.forEach((item) => insertBooksCard(item));
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+  }
+};
+
+function insertBooksCard(item) {
+  const tabela = document.getElementById("bookList");
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>${item.autor ?? "N/A"}</td>
+    <td>${item.titulo}</td>
+    <td>${item.genero}</td>
+    <td>${item.disponivel ? "Disponivel" : "Emprestado"}</td>
+    <td>${item.lancamento}</td>
+  `;
+  tabela.appendChild(tr);
+}
+
+const getBookOptions = async () => {
+  const selectBook = document.getElementById("bookOpt");
+  try {
+    const data = await BookService.getBooksList(true);
+    selectBook.innerHTML =
+      '<option value="" disabled selected hidden>Selecione um livro</option>';
+
+    data.values.forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item.id;
+      option.textContent = item.titulo;
+      selectBook.appendChild(option);
     });
-}
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para adicionar um novo item com nome, quantidade e valor 
-  --------------------------------------------------------------------------------------
-*/
-const newItem = () => {
-  let inputProduct = document.getElementById("newInput").value;
-  let inputQuantity = document.getElementById("newQuantity").value;
-  let inputPrice = document.getElementById("newPrice").value;
-
-  if (inputProduct === '') {
-    alert("Escreva o nome de um item!");
-  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-    alert("Quantidade e valor precisam ser números!");
-  } else {
-    insertList(inputProduct, inputQuantity, inputPrice)
-    postItem(inputProduct, inputQuantity, inputPrice)
-    alert("Item adicionado!")
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    selectBook.innerHTML =
+      '<option value="" disabled selected>Erro ao carregar opções</option>';
   }
-}
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para inserir items na lista apresentada
-  --------------------------------------------------------------------------------------
-*/
-const insertList = (nameProduct, quantity, price) => {
-  var item = [nameProduct, quantity, price]
-  var table = document.getElementById('myTable');
-  var row = table.insertRow();
-
-  for (var i = 0; i < item.length; i++) {
-    var cel = row.insertCell(i);
-    cel.textContent = item[i];
-  }
-  insertButton(row.insertCell(-1))
-  document.getElementById("newInput").value = "";
-  document.getElementById("newQuantity").value = "";
-  document.getElementById("newPrice").value = "";
-
-  removeElement()
-}
+};
